7fe0dd033129969e90be6f3ce81239ce8c228a01
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2021-03-16 09:12:32 -0400

    Switched to "main" branch

--- update_libigl.sh
+++ update_libigl.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 
-git subtree pull --prefix src/3rd_party/libigl https://github.com/vfonov/libigl.git master --squash
+git subtree pull --prefix src/3rd_party/libigl https://github.com/vfonov/libigl.git main --squash
