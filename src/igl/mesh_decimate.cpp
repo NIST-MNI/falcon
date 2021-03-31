afb36e1e6a53601afe989302ee0fa94f2f372b45
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2021-03-31 01:13:44 -0400

    Added option to specify fraction

--- src/igl/mesh_decimate.cpp
+++ src/igl/mesh_decimate.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-  cxxopts::Options options(argv[0], "Decimate mesh");
+  cxxopts::Options options(argv[0], "Decimate mesh, default : half");
 
   options
       .positional_help("<input> <output>")
@@ -20,32 +20,32 @@ int main(int argc, char *argv[])
     ("i,input",   "Input mesh ",   cxxopts::value<std::string>())
     ("o,output",  "Output mesh ",   cxxopts::value<std::string>())
     ("n",         "Desired number of new vertices", cxxopts::value<int>())
-    ("help", "Print help") ;
+    ("f",         "Desired fraction of new vertices", cxxopts::value<double>())
+    ("help",      "Print help") ;
   
   options.parse_positional({"input", "output"});
   auto par = options.parse(argc, argv);
 
   if( par.count("input") && 
-      par.count("output") &&
-      par.count("n") )
+      par.count("output")  )
   {
 
-    Eigen::MatrixXd V,N,UV,D,FD,ED;
+    Eigen::MatrixXd V,N,UV,VD,FD,ED;
     Eigen::MatrixXi F,E;
-    std::vector<std::string> header;
+    std::vector<std::string> headerV;
     std::vector<std::string> headerF,headerE,comments;
 
-    if(igl::readPLY(par["input"].as<std::string>(), V, F, E, N, UV, D, header,
+    if(igl::readPLY(par["input"].as<std::string>(), V, F, E, N, UV, VD, headerV,
                     FD,headerF, ED,headerE, comments))
     {
       if(par["verbose"].as<bool>()) {
         std::cout << "Vertices: " << V.rows() << "x"<< V.cols() << std::endl;
         std::cout << "Faces:    " << F.rows() << "x"<< F.cols() << std::endl;
-        std::cout << "Data:     " << D.rows() << "x"<< D.cols() << std::endl;
+        std::cout << "Data:     " << VD.rows() << "x"<< VD.cols() << std::endl;
         std::cout << "Edges:    " << E.rows() << "x"<< E.cols() << std::endl;
         std::cout<<"header:";
 
-        for(auto h:header)
+        for(auto h:headerV)
           std::cout<<h<<"\t";
         std::cout << std::endl;
       }
@@ -55,19 +55,24 @@ int main(int argc, char *argv[])
       Eigen::VectorXi J,I;
       std::vector<std::string> header1F,header1E;
 
+      int n=V.rows()/2;
+
+      if(par.count("n")) n=par["n"].as<int>();
+      else if(par.count("f")) n=par["f"].as<double>()*V.rows();
+
       //decimating
       if(!igl::decimate(V, F, par["n"].as<int>(), V1, F1, J, I))
-        std::cout<<"Failed achieving desired number of target vertices"<<std::endl;
+        std::cerr<<"Failed achieving desired number of target vertices"<<std::endl;
 
       // map data to the new vertices
       Eigen::MatrixXd D1 = 
-        Eigen::MatrixXd::NullaryExpr( V1.rows(), D.cols(), 
-          [ &I, &D]( auto r, auto c) {
-            return D(I(r),c);
+        Eigen::MatrixXd::NullaryExpr( V1.rows(), VD.cols(), 
+          [ &I, &VD]( auto r, auto c) {
+            return VD(I(r),c);
           });
 
       //TODO: populate E1!
-      if(!igl::writePLY(par["output"].as<std::string>(), V1, F1, E1, N1, UV1, D1, header,
+      if(!igl::writePLY(par["output"].as<std::string>(), V1, F1, E1, N1, UV1, D1, headerV,
                   FD1, header1F, ED1, header1E, comments, igl::FileEncoding::Binary ))
       {
         std::cerr<<"Error writing:" << par["output"].as<std::string>() << std::endl;
