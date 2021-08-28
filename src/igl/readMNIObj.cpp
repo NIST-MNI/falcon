a3f3f564f4f85822bd052390b232cc5c21e67ba7
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2021-08-28 17:44:10 -0400

    WIP: adding MNI Obj File reader/writer

---
+++ src/igl/readMNIObj.cpp
@@ -0,0 +1,101 @@
+#pragma once
+#ifndef __READ_MNIOBJ_CPP__
+#define __READ_MNIOBJ_CPP__
+
+
+#include <bicpl.h>
+
+namespace igl {
+
+template <
+  typename DerivedV,
+  typename DerivedF,
+  typename DerivedN,
+  typename DerivedC
+  >
+inline bool readPLY(
+  const std::string & fname,
+  Eigen::PlainObjectBase<DerivedV> & V,
+  Eigen::PlainObjectBase<DerivedF> & F,
+  Eigen::PlainObjectBase<DerivedN> & N,
+  Eigen::PlainObjectBase<DerivedC> & C
+  )
+{
+
+  VIO_File_formats         format;
+  object_struct        **object_list;
+  int n_objects=0;
+
+  polygons_struct      *polygons;
+
+  if( input_graphics_file( fname.c_str(), &format, &n_objects,
+                           &object_list ) != VIO_OK ) {
+    std:cerr<<"Can't open file"<<std::endl;
+    return false;
+  }
+
+  if( n_objects != 1) {
+    std::cerr<< "File "<< fname.c_str() << 
+        " contains "<< n_objects << " objects, only one is supported presently"<<std::endl;
+    return false;
+  }
+
+  if(get_object_type( object_list[0] ) != POLYGONS ) {
+    std::cerr<< "File "<<fname.c_str() << 
+        " contains unsupported object, only polygons are supported presently"<<std::endl;
+    return false;
+  }
+
+  polygons = get_polygons_ptr(object_list[0]);
+
+  n_points = polygons->n_points;
+
+  V.resize(n_points,3);
+
+  for(i=0; i<n_points; i++) {
+    V(i,0)=polygons->points[i].coords[0];
+    V(i,1)=polygons->points[i].coords[1];
+    V(i,2)=polygons->points[i].coords[2];
+  }
+
+  N.resize(n_points,3);
+
+  NIIK_RET0(((normlist=niikpt_alloc(n_points))==NULL),fcname,"niikpt_alloc");
+  for(i=0; i<n_points; i++) {
+    N(i,0)=polygons->normals[i].coords[0];
+    N(i,1)=polygons->normals[i].coords[1];
+    N(i,2)=polygons->normals[i].coords[2];
+  }
+
+  n_triangles = polygons->n_items;
+
+  if(polygons->colour_flag != ONE_COLOUR) {
+    C.resize(n_triangles,3);
+
+    for(i=0; i<n_triangles; i++) {
+
+      C(i,0)=get_Colour_r_0_1(polygons->colours[i]);
+      C(i,1)=get_Colour_g_0_1(polygons->colours[i]);
+      C(i,2)=get_Colour_b_0_1(polygons->colours[i]);
+    }
+  }
+
+  F.resize(n_triangles,3);
+  /* read/write triangles */
+  for(i=0,idx=0; i<n_triangles; i++) {
+    int j,k;
+    /*convert at most 3 points per face.... hack?*/
+    for(j=idx,k=0; j<polygons->end_indices[i] && k<3; j++,k++) {
+      F(i,k)=polygons->indices[j];
+    }
+    idx = polygons->end_indices[i];
+  }
+
+  delete_object_list( n_objects, object_list );
+
+  return true; 
+} 
+
+};//igl
+
+#endif 
---
+++ src/igl/writeMNIObj.cpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#ifndef __WRITE_MNIOBJ_CPP__
+#define __WRITE_MNIOBJ_CPP__
+
+#include  <bicpl.h>
+
+namespace igl {
+
+}
+template <
+  typename DerivedV,
+  typename DerivedF,
+  typename DerivedN,
+  typename DerivedC
+>
+bool writePLY(
+  const std::string & fname,
+  const Eigen::MatrixBase<DerivedV> & V,
+  const Eigen::MatrixBase<DerivedF> & F,
+  const Eigen::MatrixBase<DerivedN> & N,
+  const Eigen::MatrixBase<DerivedC> & C,
+   )
+{
+  object_struct   *object_list;
+  polygons_struct      *polygons;
+
+  object_list=create_object(POLYGONS);
+  polygons = get_polygons_ptr(object_list);
+
+  initialize_polygons_with_size(polygons,
+                                make_rgba_Colour(200,100,100,255),  /*reddish?*/
+                                NULL,
+                                V.rows(),F.rows(),3);
+
+  for(int i=0;i<V.rows();++i) {
+    /*assign XYZ coords*/
+    fill_Point(polygons->points[i], V(i,0),V(i,1),V(i,2));
+    fill_Point(polygons->normals[i],N(i,0),N(i,1),N(i,2));
+    /*assign normals?*/
+    i++;
+  }
+
+  /*assign indeces*/
+  for(int i=0;i<F.rows();++i) {
+    polygons->indices[i*3]  =F(i,0);
+    polygons->indices[i*3+1]=F(i,1);
+    polygons->indices[i*3+2]=F(i,2);
+  }
+  /*assume that end_indeces are properly set already*/
+
+  if(C.rows()==F.rows()) {
+    /*assign colours*/
+    set_polygon_per_item_colours(polygons);
+    for(int i=0;F.rows();++i)
+      polygons->colours[i]=make_rgba_Colour(F(i,0)*255,F(i,1)*255,F(i,2)*255,255);
+  }
+
+  output_graphics_file( fname.c_str(), ASCII_FORMAT, 1, &object_list );
+} 
+
+}; //igl
+
+#endif
